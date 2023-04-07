223a3c36921da2942f15dac41398a14a4d92306e
Victor Giraldo <117304729+Victor-M-Giraldo@users.noreply.github.com>
2023-04-07 15:51:06 -0400

    Added Navbar and Home component
    Working on frontend

---
+++ src/pages/Header.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import "../pages/Header.css";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+
+function Header() {
+    return (
+      
+      <div className='header'> 
+          
+          <img  
+              className='header_logo' 
+              src='https://i.imgur.com/njMdepp.png'
+          />
+  
+                
+          <div className='header_search'> 
+              <input className='header_searchInput' 
+              type='text' placeholder='Search Duck Exchange'/> 
+              <SearchIcon className='header_searchIcon' />
+          </div>
+  
+          <div className='header_nav'>      
+            <div className='header_option'>
+              <span 
+              className='header_optionLineOne'>
+                Hello Guest</span>
+              <span 
+              className='header_optionLineTwo'>
+                Sign In</span>
+            </div>
+  
+            <div className='header_option'>
+              <span 
+              className='header_optionLineOne'>
+                Returns</span>
+              <span 
+              className='header_optionLineTwo'>
+                & Orders</span>
+            </div>
+  
+            <div className='header_option'>
+              <span 
+              className='header_optionLineOne'>
+                Sell</span>
+              <span 
+              className='header_optionLineTwo'>
+                item</span>
+            </div>
+  
+            <div className='header_optionBasket'>
+              <ShoppingBasketIcon />
+              <span className='header_optionLineTwo header_basketCount'>0</span>
+  
+            </div>
+          </div>   
+      </div>
+    )
+  }
+  
+  export default Header
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React, {Component} from "react";
 import "../pages/Home.css";
 
-class Home extends Component{
-    render(){
-        return(
-            <div className="Home">
-                <h1>Homepage</h1>
-            </div>
-        );
-    }
-}
-export default Home;
+function Home() {
+    return (
+      <div className='home_container'>
+        <div className='home'>
+        <p>Home component
+        </p>
+        </div>
+        
+      </div>
+      
+      );
+  }
+  
+  export default Home;
