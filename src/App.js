e876720d67c127f51c732a90ea55ea431244699e
Victor Giraldo <117304729+Victor-M-Giraldo@users.noreply.github.com>
2023-04-10 23:00:10 -0400

    Fixed imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
-import Header from "./Pages/Header";
-import Home from "./Pages/Home";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
