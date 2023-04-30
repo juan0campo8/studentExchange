eba16ca3775a1aad1ec1da1699653e723b32adba
Victor Giraldo <117304729+Victor-M-Giraldo@users.noreply.github.com>
2023-04-30 08:38:30 -0700

    New pages
    Sign in, sign up, fixed the homepage a little bit. Added routing/links. Need cart page and to add functionality.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import Header from "./Header";
 import Home from "../src/pages/Home";
 import UploadItem from "./pages/uploadItem";
+import SignIn from "./pages/signin";
 
 import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import SignUp from "./pages/signup";
 
 function App() {
   return (
     <Router>
-      <Header/>
+      <Header/> 
       <Routes>
         <Route path='/Header' element = {<Header/>} />
         <Route path='/' element = {<Home/>} />
         <Route path='/uploadItem' element = {<UploadItem/>} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         
       </Routes>
     </Router>
--- src/component/items.js
+++ src/component/items.js
@@ -1,61 +1,49 @@
 import React from "react";
-import{Card, CardContent, CardMedia, Typography, Grid, Button} from "@mui/material";
-//import addToCart from "../component/AddToCart";
+import { Card, CardContent, CardMedia, Typography, Grid, Button } from "@mui/material";
+import "./items.css"
 
-// This component will be used to return a list of items and present them to the user
-  
 const Items = ({ items }) => {
-    
     const itemsList = items.length ? (
-        items.map((item) => {
-            return (
-                <Grid item xs={3}>
-                    <Card variant="outlined" style={{ width: 350, height: 375 }}>
-                        <CardMedia
-                        style={{ height: 0, paddingTop: '56.25%' }}
-                        image={item.Image_Path}
-                        title={item.Item_name}
-                        />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                        {item.Item_name}
-                        </Typography>
-                        <Typography variant="body2" color="textSecondary" component="p">
-                        {item.Item_description}
-                        </Typography>
-                        <Typography variant="h6" color="textPrimary" component="p">
-                        {`Price: $${item.Item_price}`}
-                        </Typography>
-                        <Typography variant="subtitle1" color="textSecondary" component="p">
-                            {`Category: ${item.Item_category}`}
-                        </Typography>
-                        {/*
-                            Add in a button that will add an item to a users cart
-                            <Button onClick={addToCart} 
-                        */}
-                        </CardContent>
-                    </Card>
-                </Grid>
-
-            );
-        })
+        <Grid container spacing={4} direction="row" >
+            {items.map((item) => {
+                return (
+                    <Grid item xs={3} key={item.id}>
+                        <Card variant="outlined" style={{ width: "100%", height: "100%" }} >
+                            <CardMedia
+                                style={{ height: 0, paddingTop: '56.25%' }}
+                                image={item.Image_Path}
+                                title={item.Item_name}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {item.Item_name}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {item.Item_description}
+                                </Typography>
+                                <Typography variant="h6" color="textPrimary" component="p">
+                                    {`Price: $${item.Item_price}`}
+                                </Typography>
+                                <Typography variant="subtitle1" color="textSecondary" component="p">
+                                    {`Category: ${item.Item_category}`}
+                                </Typography>
+                                {/* Add in a button that will add an item to a user's cart */}
+                                {/* <Button onClick={addToCart} /> */}
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                );
+            })}
+        </Grid>
     ) : (
-        <div className="container">
-            <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-                <p style={{ fontSize: "4rem" }}>There are no items currently available. Check back later!</p>
-            </div>
-        </div>
-        
+        <div className="center"><p className="no-items-text">No items available. Come back later!</p></div>
     );
-    return(
-        <div className="itemsCollection">
-            <Grid container spacing={4}>
-                {itemsList}        
-            </Grid>
+
+    return (
+        <div className="itemsCollection ">
+            {itemsList}
         </div>
     );
 };
 
 export default Items;
-
-
